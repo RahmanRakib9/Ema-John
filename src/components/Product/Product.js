1a412be1efe3ab5a5e744d6e2ac6208de8a22098
unknown <rahmanrakibur6589@gmail.com>
2021-09-22 14:25:18 +0600

    homePage design updated

---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+     const { name, img, seller, price, stock } = props.product;
+     return (
+          <div className='product'>
+               <div>
+                    <img src={img} alt="" />
+               </div>
+               <div>
+                    <h4 className='product-name'>{name}</h4><br />
+                    <p><small>By: {seller}</small></p>
+                    <p>$ {price}</p><br />
+                    <p><small>Only {stock} left in stock- Order soon</small></p>
+                    <button className='main-btn'><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+               </div>
+          </div>
+     );
+};
+
+export default Product;
